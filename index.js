4b057aa15930c8f1268c1826bd6276307c1d0cb2
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-11-10 20:18:06 +0100

    Updates

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-import App2 from './App.js';
-import __SNOWPACK_ENV__ from './env.js';
-import { RootProvider } from './providers/index.js';
-import ReactDOM from './web_modules/react-dom.js';
-import React from './web_modules/react.js';
+import __SNOWPACK_ENV__ from './__snowpack__/env.js';
 import.meta.env = __SNOWPACK_ENV__;
 
-ReactDOM.render(
-  /* @__PURE__ */ React.createElement(
-    React.StrictMode,
-    null,
-    /* @__PURE__ */ React.createElement(
-      RootProvider,
-      null,
-      /* @__PURE__ */ React.createElement(App2, null),
-    ),
-  ),
-  document.getElementById('root'),
-);
+import React from "./web_modules/react.js";
+import ReactDOM from "./web_modules/react-dom.js";
+import App2 from "./App.js";
+import {RootProvider} from "./providers/index.js";
+ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(RootProvider, null, /* @__PURE__ */ React.createElement(App2, null))), document.getElementById("root"));
 if (import.meta.hot) {
   import.meta?.hot?.accept();
 }
